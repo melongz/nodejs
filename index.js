8e8deb5ec184a0908bd56c073a5097ea9e8d4673
melongz <1945857873@qq.com>
2020-10-23 17:50:50 +0800

    feat: 学习node基本模块使用

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const http = require('http');
-const fs = require('fs');
 const url = require('url');
-const querystring = require('querystring');
-const rule = require('./src/rule');
+const fs = require('fs');
+const path = require('path');
+
+// const testPath = path.join(__dirname, '/source/test');
+// async function mkdirTest (dirPath, fileName, content) {
+//   await fs.mkdir(dirPath, { recursive: true, }, err => null);
+//   await fs.writeFile(dirPath + '/' + fileName, content, err => null);
+//   return Promise.resolve(dirPath + '/' + fileName);
+// }
+// mkdirTest(testPath, 'test2.js', 'console.log(666);')
+//   .then(res => {
+//     fs.appendFile(res, 'console.log(555777);', err => null )
+//   })
+const filePath = path.join(__dirname, '/source/log.txt');
+const readerStream = fs.createReadStream(filePath, {encoding: 'UTF8'});
+let count = 0;
+readerStream.on('data', chunk => {
+  console.log(chunk);
+  count += 1;
+})
+readerStream.on('end', res => console.log(12345, res, count))
+
 
 http
-  .createServer(function (req, res) {
-    const {pathname, query} = url.parse(req.url)
-    if (pathname === '/favicon.ico') {
+  .createServer(((req, res) => {
+    const reqUrl = req.url;
+    if (reqUrl === '/favicon.ico') {
       res.writeHead(200);
       res.end();
       return;
     }
-    if (pathname === '/') {
-      res.writeHead(200);
-      fs.createReadStream(__dirname + '/source/index.html')
-        .pipe(res);
-    }
-    if (pathname === '/game') {
-      res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
-      const key = querystring.parse(query).id;
-      const result = rule(key);
-      res.end(JSON.stringify(result));
-    }
-  })
+    const {name, age} = url.parse(reqUrl, true).query;
+    res.writeHead(200,
+      {
+        'Content-Type': 'text/html;charset=UTF-8',
+      }
+    )
+    res.write(`<div>姓名：${name}<br>年龄：${age}</div>`);
+    res.end();
+  }))
   .listen(3000);
