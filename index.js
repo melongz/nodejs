8621c001d0dafb481622316df9ca442df7a1e8dd
melongz <1945857873@qq.com>
2020-10-26 17:49:11 +0800

    feat: node方法修改

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const path = require('path');
 
 const extData = fs.readFileSync('./config/ext.json');
 const EXT_MAP = JSON.parse(extData.toString());
+const mockData = {
+  status: 200,
+  data: {
+    name: 'melongz',
+    age: 27,
+  },
+  message: '请求成功'
+}
 
-function noFileSlove (res) {
+function handleNoFile (res) {
   fs.readFile('./web/pages/404/index.html', (err, fileData) => {
     if (err) {
       res.statusCode = 404
@@ -14,6 +22,61 @@ function noFileSlove (res) {
     } else {
       res.statusCode = 200;
       res.setHeader('Content-type', 'text/html;charset=UTF-8');
+      res.write(fileData);
+      res.end();
+    }
+  })
+}
+
+function handleFavicon (res) {
+  fs.readFile('./web/favicon.ico', (err, fileData) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+    } else {
+      res.statusCode = 200;
+      res.write(fileData);
+      res.end();
+    }
+  })
+}
+
+function handleApi (req, res) {
+  const method = req.method;
+  const { query, } = url.parse(req.url, true);
+  let reqParams = '';
+  res.setHeader('Content-Type', 'application/json');
+  const handleMethod = {
+    'OPTIONS': function () {
+      res.statusCode = 200;
+    },
+    'GET': function () {
+      res.statusCode = 200;
+      res.write(JSON.stringify(mockData));
+    },
+    'POST': function () {
+      req.on('data', chunk => {
+        console.log(222, chunk);
+        reqParams += chunk;
+      })
+      req.on('end', () => {
+
+      })
+      res.statusCode = 200;
+      res.write(JSON.stringify(mockData));
+    }
+  }
+  handleMethod[method]();
+  res.end();
+}
+
+function handleHtml (res, pathname) {
+  fs.readFile(`./web/pages${pathname}`, (err, fileData) => {
+    if (err) {
+      handleNoFile(res);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', getContentType(pathname));
       res.write(fileData);
       res.end();
     }
@@ -28,30 +91,18 @@ function getContentType (pathname) {
 http
   .createServer(((req, res) => {
     const reqUrl = req.url;
-    if (reqUrl.includes('/favicon.ico')) {
-      fs.readFile('./web/favicon.ico', (err, fileData) => {
-        if (err) {
-          res.statusCode = 404;
-          res.end();
-        } else {
-          res.statusCode = 200;
-          res.write(fileData);
-          res.end();
-        }
-      })
+    const urlInfo = url.parse(reqUrl, true);
+    const pathname = urlInfo.pathname;
+    const isApi = reqUrl.slice(0, 4) === '/api';
+    const isFavicon = pathname.includes('/favicon.ico');
+    if (isFavicon) {
+      handleFavicon(res);
       return;
     }
-    const urlInfo = url.parse(reqUrl, true);
-    const pathname = urlInfo.pathname === '/' ? '/home/index.html' : urlInfo.pathname;
-    fs.readFile(`./web/pages${pathname}`, (err, fileData) => {
-      if (err) {
-        noFileSlove(res);
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', getContentType(pathname));
-        res.write(fileData);
-        res.end();
-      }
-    })
+    if (isApi) {
+      handleApi(req, res);
+      return;
+    }
+    handleHtml(res, pathname);
   }))
   .listen(3000);
--- web/pages/home/index.js
+++ web/pages/home/index.js
@@ -1,1 +1,10 @@
 console.log('melongz');
+fetch('/api/personInfo', {
+  method: 'POST',
+  body: JSON.stringify({name: 111, age: 222}),
+  headers: new Headers({
+    'Content-Type': 'application/json'
+  })
+})
+  .then(res => res.json())
+  .then(res => console.log(res))
