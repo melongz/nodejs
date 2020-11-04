332683b16d2d4714f8effea0c9ea5e9a1f348712
melongz <1945857873@qq.com>
2020-11-04 09:39:40 +0800

    feat: koa学习

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const path = require('path');
 const events = require('events');
 const mongoClient = require('mongodb');
 const Koa = require('koa');
-const router = require('koa-router');
+const Router = require('koa-router'); // 路由
+const static = require('koa-static'); // 加载静态资源
+const bodyparser = require('koa-bodyparser'); // 解析post请求参数
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
+router.get('/', async (ctx, next) => {
   await next();
-  console.log(1122);
-  ctx.res.type = 'text/html';
-  ctx.body = '<h1>hello world</h1>';
+  ctx.body = fs.readFileSync('./web/pages/home/index.html', 'utf8');
+})
+
+// app.use(static(__dirname, '/static'));
+
+app.use(bodyparser());
+
+app.use(router.routes());
+
+app.listen(3000, () => {
+  console.log('Koa is listening in http://localhost:3000')
 });
-
-app.use(async (ctx, next) => {
-  await next();
-  console.log(3344);
-})
-
-app.use(async (ctx, next) => {
-  console.log(5566);
-})
-
-app.listen(3000);
 
