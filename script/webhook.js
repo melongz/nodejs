eb3f832ffc9a1e801ed2edc9b88007fdeef797c0
melongz <1945857873@qq.com>
2020-11-06 22:28:15 +0800

    feat: 测试自动化部署

--- script/webhook.js
+++ script/webhook.js
@@ -17,7 +17,7 @@ handler.on('error', function (err) {
 
 handler.on('push', function (event) {
   // 保证是main分支的push行为才会去更新代码
-  console.log('webhook', 123,  new Date());
+  console.log('webhook', 123);
   if (event.payload && event.payload.ref === 'refs/heads/main') {
     console.log('pull');
     runCommand('sh', ['./deploy.sh'], function( txt ){
